0476d1a760bd6b7bcf1c7c095f57998a69de7290
Danil Maximov <jokjokjokerface@gmail.com>
2020-10-03 02:49:55 +0300

    init bot

---
+++ bot.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+	"reflect"
+
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
+	"github.com/joho/godotenv"
+)
+
+//Bot create logic for work with telegram api.
+func Bot() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if reflect.TypeOf(update.Message.Text).Kind() == reflect.String && update.Message.Text != "" {
+
+			switch update.Message.Text {
+			case "/start":
+
+				msg := tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Привет, я бот для закупок, че ты хочешь...")
+				bot.Send(msg)
+			case "/help":
+				msg := tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Это помощь!")
+				bot.Send(msg)
+			}
+		}
+	}
+}
+
+// init is invoked before main()
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print(" No .env file found!")
+	}
+}
+
+// main function
+func main() {
+	// Call Bot
+	Bot()
+}
