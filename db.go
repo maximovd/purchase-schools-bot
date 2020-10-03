d8e44e418ae78ce6eee3d658b74226b5d0cdad80
Danil Maximov <jokjokjokerface@gmail.com>
2020-10-04 02:47:01 +0300

    add GetAllCategoru func

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/lib/pq"
@@ -16,8 +17,14 @@ var dbname = os.Getenv("DBNAME")
 
 var dbinfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", host, port, user, password, dbname)
 
-//CreateTable category table in database
-func CreateTable() error {
+// PurchaseCategory struct for
+type PurchaseCategory struct {
+	ID   int
+	name string
+}
+
+// CreateTables category and purchases tables in database
+func CreateTables() error {
 
 	db, err := sql.Open("postgres", dbinfo)
 	if err != nil {
@@ -25,7 +32,7 @@ func CreateTable() error {
 	}
 	defer db.Close()
 
-	//Create category Table
+	// Create category Table
 	if _, err = db.Exec(
 		`CREATE TABLE IF NOT EXISTS purchase_categories(category_id SERIAL PRIMARY KEY, category_name TEXT NOT NULL);`,
 	); err != nil {
@@ -46,4 +53,31 @@ func CreateTable() error {
 	}
 
 	return nil
+}
+
+// GetAllCategory from database
+func GetAllCategory() ([]string, error) {
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	var category PurchaseCategory
+	var AllCategories []string
+
+	categories, err := db.Query("SELECT * FROM purchase_categories;")
+	if err != nil {
+		log.Fatal("Failed to execute query: ", err)
+	}
+
+	for categories.Next() {
+		if err := categories.Scan(&category.ID, &category.name); err != nil {
+			log.Fatal(err)
+		}
+		AllCategories = append(AllCategories, category.name)
+	}
+	return AllCategories, err
 }
